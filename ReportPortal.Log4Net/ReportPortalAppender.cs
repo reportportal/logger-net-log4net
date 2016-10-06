b2ed0cb8d0fe2fec804dabfb426a90d679c0595e
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2016-10-06 23:04:24 +0300

    Added sources

---
+++ ReportPortal.Log4Net/ReportPortalAppender.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using ReportPortal.Client.Models;
+using ReportPortal.Client.Requests;
+using ReportPortal.Shared;
+using log4net.Appender;
+using log4net.Core;
+
+namespace ReportPortal.Log4Net
+{
+    /// <summary>
+    /// Log4Net custom adapter for reporting logs directly to Report Portal.
+    /// Logs will be viewable under current test item from shared context.
+    /// </summary>
+    public class ReportPortalAppender: AppenderSkeleton
+    {
+        protected Dictionary<Level, LogLevel> LevelMap = new Dictionary<Level, LogLevel>();
+        public ReportPortalAppender()
+        {
+            LevelMap[Level.Fatal] = LogLevel.Error;
+            LevelMap[Level.Error] = LogLevel.Error;
+            LevelMap[Level.Warn] = LogLevel.Warning;
+            LevelMap[Level.Info] = LogLevel.Info;
+            LevelMap[Level.Debug] = LogLevel.Debug;
+            LevelMap[Level.Trace] = LogLevel.Trace;
+        }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            if (Bridge.Context.TestId != null)
+            {
+                var level = LogLevel.Info;
+                if (LevelMap.ContainsKey(loggingEvent.Level))
+                {
+                    level = LevelMap[loggingEvent.Level];
+                }
+
+                var message = loggingEvent.RenderedMessage;
+
+                if (loggingEvent.ExceptionObject != null)
+                {
+                    message += Environment.NewLine + loggingEvent.GetExceptionString();
+                }
+
+                var request = new AddLogItemRequest
+                {
+                    TestItemId = Bridge.Context.TestId,
+                    Level = level,
+                    Time = DateTime.UtcNow,
+                    Text = message
+                };
+                
+
+                Bridge.Service.AddLogItem(request);
+            }
+        }
+    }
+}
